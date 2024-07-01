5d691eb8dd414c027967f940f61a1b7a0d105ca2
ryanowino <owinoryan@gmail.com>
2024-07-01 12:03:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+
+
+const random = Math.ceil(Math.random() * 100); 
+
+console.log(random); 
+
+
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+console.log(mod);console.log(mod);
+
+
+
+const max = Math.max(5, 10, 15, 20);
+
+console.log(max); 
+
+
+
 
 // Write your code here
